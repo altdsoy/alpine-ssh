b1be5f18d295cefff1ca861e46ef30d7716313e5
Alt D. Soy <altdsoy@gmail.com>
2020-10-09 01:31:51 +0200

    Fix path typo

--- set_ssh_config.sh
+++ set_ssh_config.sh
@@ -5,6 +5,6 @@ mkdir -p ~/.ssh
 chmod 700 ~/.ssh
 echo -e "Host *\n\tStrictHostKeyChecking no\n\tIdentityFile ~/ssh_key\n\n" > ~/.ssh/config
 echo -e "$SSH_PRIVATE_KEY" > ~/ssh_key
-chmod 600 ~/.ssh/ssh_key
+chmod 600 ~/ssh_key
 
 exec "$@"
